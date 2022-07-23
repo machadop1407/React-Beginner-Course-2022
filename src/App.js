b3e8777d88a6a812ca510648ba64d155a9a1d6db
Pedro Henrique Ferraz Machado <pedrotech@Pedros-Mac-Studio.local>
2022-07-23 15:41:55 -0700

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
-import { useState } from "react";
-import { Text } from "./Text";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [showText, setShowText] = useState(false);
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
+
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`).then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse);
+      }
+    );
+  };
 
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setShowText(!showText);
-        }}
-      >
-        Show Text
-      </button>
+      <h1> Generate An Excuse </h1>
+      <button onClick={() => fetchExcuse("party")}> Party</button>
+      <button onClick={() => fetchExcuse("family")}> Family</button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
 
-      {showText && <Text />}
+      <p> {generatedExcuse} </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
