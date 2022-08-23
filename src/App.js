a4d4533d75613095e4cb003cff264836bd4a280d
Pedro Henrique Ferraz Machado <pedrotech@Pedros-Mac-Studio.local>
2022-08-23 15:46:43 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import { useToggle } from "./useToggle";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+      },
+    },
+  });
   return (
     <div className="App">
-      <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
-      {isVisible && <h1> Hidden text</h1>}
+      <QueryClientProvider client={client}></QueryClientProvider>
     </div>
   );
 }
