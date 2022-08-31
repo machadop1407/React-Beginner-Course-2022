d997aff8171a7b4aa1fa9cb416d9d584a8f6f701
Pedro Henrique Ferraz Machado <pedrotech@Pedros-Mac-Studio.local>
2022-08-30 17:54:40 -0700

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Contact } from "./pages/Contact";
-import { Login } from "./pages/Login";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Router>
-          <Link to="/"> Home </Link>
-          <Link to="/login"> Login </Link>
-          <Link to="/contact"> Contact </Link>
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/People.ts
+++
@@ -1,9 +0,0 @@
-export const People = [
-  {
-    name: "Pedro",
-    email: "email@email.com",
-    age: 21,
-    isMarried: true,
-    friends: ["jessica"],
-  },
-];
